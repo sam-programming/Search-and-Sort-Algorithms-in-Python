e81e050c7b785b03744add485a9ffd8df7f91ee0
Samuel Warner <72848532+sam-programming@users.noreply.github.com>
2021-03-18 15:55:36 +0800

    Update search_algorithms.py

--- search_algorithms.py
+++ search_algorithms.py
@@ -73,17 +73,90 @@ def Is_Ascending(array):               # O(1)
 
 # Maximum number of comparisons = n - 1
 # Time complexity max{ O(n - 1), O(n - 1) }, O(n)
-    
+
+# Sort and Merge arrays where array 1 and array 2 are
+# already sorted
+
+def Merge(arr1, arr2):
+    i = 0 # loop control for array 1
+    j = 0 # loop control for array 2
+    arr3 = []
+    while i < len(arr1) and j < len(arr2): #will terminate when 1 condition fails
+        if arr1[i] <= arr2[j]:
+            arr3.append(arr1[i])
+            i += 1
+        elif arr1[i] > arr2[j]:
+            arr3.append(arr2[j])
+            j += 1
+    #at this point 1 of the arrays should have elements that have not been merged
+    #here we find which array that is and add the remaining elements to arr3    
+    while i <= len(arr1)-1:   
+        arr3.append(arr1[i])
+        i += 1
+    while j <= len(arr2)-1:
+        arr3.append(arr2[j])
+        j += 1    
+    return arr3
+
+
+# Swap two values in an array
+def Swap(arr, i1, i2): #array, index 1, index 2
+    temp = arr[i1]
+    arr[i1] = arr[i2]
+    arr[i2] = temp
+
+#Selection Sort -  not hitting last element
+#O(n^2)
+def Selection_Sort(arr):
+    for i in range(len(arr)): # set the left side of the array
+        min_index = i #initialise the minimum
+        for x in range(i+1, len(arr)): #iterate over array starting at left
+            if arr[min_index] > arr[x]: #compare current min value with the rest of the array
+                min_index = x  #if less, new min_index
+        if min_index != i:  #if the min is not the current value of arr[i]
+            arr[i], arr[min_index] = arr[min_index], arr[i] #swap that shit
+                
+
+
+#Insertion Sort
+def Insertion_Sort(arr):
+    left = 0
+    for index in range(len(arr)-1):
+        right = index + 1 # set right boundary
+        val = arr[right]  # get value at right boundary
+        x = left # loop control
+        while x < right:  #starting at 0th index
+            if val < arr[x]:  #if the index is ever less than the array
+                arr.remove(val) #remove the value at index
+                arr.insert(x, val) #insert the value at new index
+                x = right          #terminate while loop
+            x += 1  #otherwise, iterate while loop
+            
+
 def Is_Palindrome(word):
     reverse = ""
     for letter in word:
         reverse
         
-       
-print(Is_Ascending(word_arr))
-Binary_Search('elfs', word_arr)
-print(Linear_Search('seldom', word_arr))
-print(Linear_Search_Min(word_arr))
-print(Linear_Search_Max(word_arr))
-        
+
+#RUN THE FUNCTIONS!!!
+                    
+s_arr1 = ['cow', 'goat']
+s_arr2 = ['cat', 'dog', 'fox', 'lion', 'tiger']
+arr1 = ['milk']
+arr2 = ['kitten']
+us_arr = ['fox', 'cow', 'pig', 'cat', 'rat', 'lion', 'tiger', 'goat', 'dog']
+us_int_arr = [23, 56, 7, 44, 768, 90, 107, 22, 45, 66, 88, 1, 12]
+merged_arr = Merge(arr1, arr2)
+#print(merged_arr)
+
+#Selection_Sort(us_int_arr)
+#Insertion_Sort(us_int_arr)
+#print(us_int_arr)
+#print(Is_Ascending(word_arr))
+#Binary_Search('elfs', word_arr)
+#print(Linear_Search('seldom', word_arr))
+#print(Linear_Search_Min(word_arr))
+#print(Linear_Search_Max(word_arr))
+      
         
